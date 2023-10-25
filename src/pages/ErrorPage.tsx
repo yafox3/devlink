e2f4ea7aafd2c9df16f5f224d515e0c7cfdaf75d
yafox3 <ruslan.kret@yandex.ru>
2023-10-25 16:39:28 +0300

    made an error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage: FC = () => {
-	return <div>Error</div>
+	return <div className='min-h-screen font-poppins flex flex-col justify-center items-center select-none'>
+		<span className='text-[14rem] mb-36'>😔</span>
+		<p className='text-4xl mb-8'>
+			Page not found!
+		</p>
+		<Link to='/' className='btn'>Go back</Link>
+	</div>
 }
 
 export { ErrorPage }
