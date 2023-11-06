acf7392ef3c5c4117964f0263615bad866320fe2
yafox3 <ruslan.kret@yandex.ru>
2023-11-06 10:26:20 +0300

    changed export type

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -11,16 +11,16 @@ const Modal: FC<ModalProps> = ({ children, isOpen, setIsOpen }) => {
 			className={'fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black/50 transition-all duration-300 '.concat(
 				isOpen ? 'visible opacity-100' : 'invisible opacity-0'
 			)}
-			onClick={() => setIsOpen(false)}
-		>
-				<div 
-					className={'w-full max-w-md max-h-full py-5 px-10 bg-white rounded-xl transition duration-700 '.concat(isOpen ? 'translate-y-0' : '-translate-y-[100vh]')}
-					onClick={(event: React.MouseEvent) => event.stopPropagation()}
-				>
-					{children}
-				</div>
+			onClick={() => setIsOpen(false)}>
+			<div
+				className={'w-full max-w-md max-h-full py-5 px-10 bg-white rounded-xl transition duration-700 '.concat(
+					isOpen ? 'translate-y-0' : '-translate-y-[100vh]'
+				)}
+				onClick={(event: React.MouseEvent) => event.stopPropagation()}>
+				{children}
 			</div>
+		</div>
 	)
 }
 
-export default Modal
+export { Modal }
