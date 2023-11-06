4f82dfb9fa12b3ab636dd2eece21bddde2426ddf
yafox3 <ruslan.kret@yandex.ru>
2023-11-06 12:25:26 +0300

    made a profile page without functionality

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,43 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { ImageUpload, Input } from '../components'
 
 const Profile: FC = () => {
-	return <div>Profile</div>
+	const [currentImage, setCurrentImage] = useState<File | null>(null)
+	const [previewImage, setPreviewImage] = useState<string>('')
+
+	return (
+		<div className='py-10 max-w-[400px] flex flex-col justify-center items-center mx-auto'>
+			<h1 className='title mb-8'>Profile</h1>
+
+			<form action='submit'>
+				<ImageUpload
+					previewImage={previewImage}
+					setCurrentImage={setCurrentImage}
+					setPreviewImage={setPreviewImage}
+				/>
+
+				<div className='mx-auto text-center mb-10'>
+					<h5 className='mb-2'>Profile picture</h5>
+					<p className='text-black/60'>
+						Image must be below 1024x1024px. <br />
+						Use PNG, JPG format
+					</p>
+				</div>
+
+				<div className='flex flex-col gap-5 w-full mb-16'>
+					<Input
+						className='min-w-[400px]'
+						placeholder='Enter your first name here'
+						label='First Name'
+					/>
+					<Input className='w-full' placeholder='Enter your last name here' label='Last Name' />
+				</div>
+
+				<button className='btn block mx-auto'>Save changes</button>
+			</form>
+		</div>
+	)
 }
 
 export { Profile }
+
