0dfb78cc655b7d88007ba73ba6661af4949afcdb
yafox3 <ruslan.kret@yandex.ru>
2023-11-08 14:06:53 +0300

    added fields to models

--- src/models/link.ts
+++ src/models/link.ts
@@ -1,6 +1,7 @@
 import { Platforms } from './platforms'
 
 export interface ILink {
+	id: number
 	link: string
 	platform: Platforms
 }
--- src/models/platforms.ts
+++ src/models/platforms.ts
@@ -1,5 +1,6 @@
 export enum Platforms {
 	GITHUB = 'GitHub',
 	YOUTUBE = 'Youtube',
-	LINKEDIN = 'LinkedIn'
+	LINKEDIN = 'LinkedIn',
+	NONE = ''
 }
