e0b59236ba6541817bc78521916ac13c005ca48e
yafox3 <ruslan.kret@yandex.ru>
2023-11-12 13:27:06 +0300

    made a preview component for devcard editor

---
+++ src/components/DevCardPreview.tsx
@@ -0,0 +1,83 @@
+import { FC, ReactNode } from 'react'
+import { ILink, Platforms } from '../models'
+import { FaArrowRight, FaGithub, FaLinkedin, FaYoutube } from 'react-icons/fa6'
+
+interface DevCardPreviewProps {
+	email: string
+	firstName: string
+	lastName: string
+	img: string
+	links: ILink[]
+}
+
+const DevCardPreview: FC<DevCardPreviewProps> = ({email, firstName, img, lastName, links}) => {
+	const platformToJSX = (platform: Platforms): ReactNode => {
+		switch (platform) {
+			case Platforms.GITHUB:
+				return (
+					<div className='flex items-center justify-between h-14 w-64 bg-black/90 p-5 text-white rounded-xl'>
+						<div className='flex items-center gap-4'>
+							<FaGithub className='text-lg'/>
+							{Platforms.GITHUB}
+						</div>
+						<FaArrowRight className='text-lg'/>
+					</div>
+				)
+			case Platforms.YOUTUBE:
+				return (
+					<div className='flex items-center justify-between h-14 w-64 bg-red-700 p-5 text-white rounded-xl'>
+						<div className='flex items-center gap-4'>
+							<FaYoutube className='text-lg'/>
+							{Platforms.YOUTUBE}
+						</div>
+						<FaArrowRight className='text-lg'/>
+					</div>
+				)
+			case Platforms.LINKEDIN:
+				return (
+					<div className='flex items-center justify-between h-14 w-64 bg-blue-600 p-5 text-white rounded-xl'>
+						<div className='flex items-center gap-4'>
+							<FaLinkedin className='text-lg'/>
+							{Platforms.LINKEDIN}
+						</div>
+						<FaArrowRight className='text-lg'/>
+					</div>
+				)
+			default: return <div className='h-14 w-64 bg-black/10 rounded-2xl animate-pulse'></div>
+		}
+	}
+
+	return (
+		<div className='relative select-none z-10 w-[344px] h-[683px] border-[16px] border-gray-900 rounded-[55px] shadow-xl overflow-hidden'>
+			<div className='absolute -top-2 left-1/2 -translate-x-1/2 w-32 h-6 bg-gray-900 rounded-2xl'></div>
+
+			<div className='mt-10'>
+				{img 
+					? <img className='mb-4 w-28 h-28 mx-auto rounded-full' src={img} alt='devlink avatar' />
+					: <div className='mb-4 w-28 h-28 mx-auto rounded-full bg-black/10 animate-pulse'></div>
+				}
+
+				{firstName && lastName 
+					? <p className='text-center font-bold text-lg mb-2'>{firstName} {lastName}</p>
+					: <div className='h-3 bg-black/10 rounded-full w-48 mb-4 mx-auto animate-pulse'></div>
+				}
+
+				{email 
+					? <p className='text-center text-black/75 mb-14'>{email}</p>
+					: <div className='h-2.5 bg-black/10 rounded-full w-24 mb-14 mx-auto animate-pulse'></div>
+				}
+
+				<div className='flex flex-col items-center gap-6'>
+					{links.map(link => (
+						<div key={link.id}>
+							{platformToJSX(link.platform)}
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export { DevCardPreview }
+
