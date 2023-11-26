e919cfff9aa00eb04641ca01087aa346556f39ab
yafox3 <ruslan.kret@yandex.ru>
2023-11-26 11:56:16 +0300

    removed converter from url to file

--- src/helpers/urlToFile.helper.ts
+++
@@ -1,5 +0,0 @@
-export const urlToFile = async (url: string, filename: string = 'image', mimeType: string = 'image/png') => {
-	const res = await fetch(url);
-	const blob = await res.blob();
-	return new File([blob], filename, {type: mimeType});
-}
