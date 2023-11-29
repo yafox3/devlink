1540839be8f253c4fa5dbae6b8262348627b2395
yafox3 <ruslan.kret@yandex.ru>
2023-11-29 10:59:42 +0300

    added loader to dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,14 @@
 import { FC, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { DevCardList } from '../components'
+import { DevCardList, Spinner } from '../components'
 import { useAppSelector } from '../hooks'
-import { IDevCard } from '../models'
+import { IDevCard, Statuses } from '../models'
 
 const Dashboard: FC = () => {
 	const [devCards, setDevCards] = useState<IDevCard[]>([])
 	const userDevCards = useAppSelector(state => state.user.user.devCards)
+	const status = useAppSelector(state => state.user.status)
+	const isLoading = status === Statuses.LOADING
 	const navigate = useNavigate()
 
 	useEffect(() => {
@@ -20,8 +22,15 @@ const Dashboard: FC = () => {
 	return (
 		<div className='py-10'>
 			<h1 className='title mb-16'>Links</h1>
-			<DevCardList devCards={devCards} />
-			<button className='btn block mx-auto' onClick={createLinkHandler}>
+			{isLoading ? (
+				<div className='w-10 h-10 mx-auto mb-16'>
+					<Spinner className='w-10 h-10 dark:fill-blue-600 dark:text-black/5 fill-blue-600 text-black/5' />
+				</div>
+			) : (
+				<DevCardList devCards={devCards} />
+			)}
+
+			<button disabled={isLoading} className='btn block mx-auto' onClick={createLinkHandler}>
 				Create link
 			</button>
 		</div>
