844da9014bf4af9df29d6924d2bf00ffe4aebe5c
yafox3 <ruslan.kret@yandex.ru>
2023-12-02 11:54:32 +0300

    added an interceptor to axios

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,9 +3,12 @@ import { getTokenFromLocalStorage } from '../helpers'
 
 const instance = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
-	headers: {
-		Authorization: `Bearer ${getTokenFromLocalStorage()}`
-	}
+})
+
+instance.interceptors.request.use((config) => {
+	const token = getTokenFromLocalStorage()
+	config.headers.Authorization = `Bearer ${token}`
+	return config
 })
 
 export { instance as axios }
