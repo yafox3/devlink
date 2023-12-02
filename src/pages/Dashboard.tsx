2d37d215124b9e76ab83db1a015cd8b45243e9d2
yafox3 <ruslan.kret@yandex.ru>
2023-12-02 12:31:27 +0300

    added state to not founded devcards, changed method to recieve devcards

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,21 @@
 import { FC, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { DevCardList, Spinner } from '../components'
-import { useAppSelector } from '../hooks'
+import { useAppDispatch, useAppSelector } from '../hooks'
 import { IDevCard, Statuses } from '../models'
+import { getUserCards } from '../store/slices/userSlice'
 
 const Dashboard: FC = () => {
 	const [devCards, setDevCards] = useState<IDevCard[]>([])
 	const userDevCards = useAppSelector(state => state.user.user.devCards)
 	const status = useAppSelector(state => state.user.status)
+	const dispatch = useAppDispatch()
 	const isLoading = status === Statuses.LOADING
 	const navigate = useNavigate()
+
+	useEffect(() => {
+		userDevCards.length || dispatch(getUserCards())
+	}, [])
 
 	useEffect(() => {
 		setDevCards(userDevCards)
@@ -26,8 +32,10 @@ const Dashboard: FC = () => {
 				<div className='w-10 h-10 mx-auto mb-16'>
 					<Spinner className='w-10 h-10 dark:fill-blue-600 dark:text-black/5 fill-blue-600 text-black/5' />
 				</div>
-			) : (
+			) : devCards.length ? (
 				<DevCardList devCards={devCards} />
+			) : (
+				<p className='text-center text-2xl mb-16'>Nothing found</p>
 			)}
 
 			<button disabled={isLoading} className='btn block mx-auto' onClick={createLinkHandler}>
